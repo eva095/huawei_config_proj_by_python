be3f966f583580b8a27c0103479c20dfff8ecfb5
eva095 <160281305+eva095@users.noreply.github.com>
2025-01-29 00:39:41 +0300

    Update 02_mstp_main.py

--- scripts/02_mstp_main.py
+++ scripts/02_mstp_main.py
@@ -2,7 +2,7 @@ from data.devices import HUAWEI_SWITCHES
 from data.stp_data import STP_CONFIGS
 from core.mstp_logic import STPConfigurator
 
-INTERFACE_TO_DISABLE = "GE 1/0/2"  # Указываем интерфейс для отключения
+INTERFACE_TO_DISABLE = "GE 1/0/2"  
 
 for device in HUAWEI_SWITCHES:
     host = device["host"]
@@ -15,9 +15,9 @@ for device in HUAWEI_SWITCHES:
     configurator = STPConfigurator(device, stp_commands)
     try:
         configurator.connect()
-        configurator.configure_stp()  # Настройка STP
-        configurator.disable_interface(INTERFACE_TO_DISABLE)  # Отключение интерфейса
-        configurator.commit_changes()  # Применение изменений
+        configurator.configure_stp()  
+        configurator.disable_interface(INTERFACE_TO_DISABLE)  
+        configurator.commit_changes()  
     except Exception as e:
         print(f"Ошибка на устройстве {host}: {e}")
     finally:
